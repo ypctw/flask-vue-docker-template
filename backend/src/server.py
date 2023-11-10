f2c741d6f9110456498abc6b534797e97f586e82
Yen Po, Chen <kevinkyle880821@gmail.com>
2023-11-10 09:19:16 +0800

    Update server.py

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,15 +1,9 @@
 from flask import Flask, render_template
 from flask_cors import CORS
 
-
 app = Flask(__name__,template_folder="./template")
 cors = CORS(app)
-
-
-# inputPath recommand : "/usr/src/LeArNER/input/{}.txt"
 
 @app.route('/')
 def homePage():
     return render_template('homepage.html')
-
-
